2fac38f9150db2a6d0f79a958a63a96f4185763b
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2024-06-29 10:55:00 -0400

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -18,13 +18,6 @@ alias ll="ls -alFh"
 # color highlighting to grep output
 alias grep='grep --color=auto'
 
-# ping -> mtr
-# (use ping4 and ping6 for basic ping)
-alias ping="mtr"
-
-# get your external ip
-alias myip="curl ifconfig.me"
-
 # apt hints
 alias apt-sources="sudo nano /etc/apt/sources.list"
 alias apt-search="apt-cache --names-only search"
@@ -33,10 +26,7 @@ alias apt-info="apt-cache showpkg"
 # docker hints
 alias dc-up="docker compose -f /opt/valinor/(hostname)-compose.yml up -d \$argv"
 alias dc-down="docker compose -f /opt/valinor/(hostname)-compose.yml down \$argv"
-alias dc-logs="docker compose -f /opt/valinor/(hostname)-compose.yml logs \$argv"
-alias dc-edit="nano /opt/valinor/(hostname)-compose.yml"
 alias dc-config="docker compose -f /opt/valinor/(hostname)-compose.yml config"
-alias dc-restart="docker compose -f /opt/valinor/(hostname)-compose.yml restart \$argv"
 alias dc-pull="docker compose -f /opt/valinor/(hostname)-compose.yml pull \$argv"
 alias dc-build="docker compose -f /opt/valinor/(hostname)-compose.yml build \$argv"
 alias dc-sync="cd /opt/valinor; git pull; dc-pull; dc-up"
